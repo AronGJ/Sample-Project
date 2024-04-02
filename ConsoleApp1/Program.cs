ec7590a326bb91916741333434d5be0c2f4e6569
Aron Granada <aronjustin.granada@gmail>
2024-04-02 20:35:08 +0800

    extracted discounted prices from steam page

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Sample");
 
 Console.WriteLine("Sample2");
 
+var url = "https://store.steampowered.com/search/?filter=topsellers";
+var web = new HtmlWeb();
+var doc = web.Load(url);
+var discountedPrices = doc.DocumentNode.SelectNodes("//div[@class='discount_final_price']");
+
+foreach (var price in discountedPrices)
+{
+    Console.WriteLine(price.InnerText);
+
+}
+
+
 Console.ReadLine();
+
+
